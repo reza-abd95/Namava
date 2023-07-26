816597ed163628368dac35ce1d7c99b29c5bb0ac
Mehrdad.Pashaei <mehrdad.pashaei1@gmail.com>
2023-07-26 14:33:14 -0700

    feat (header) : add some changes.

--- app/(components)/Header.js
+++ app/(components)/Header.js
@@ -10,18 +10,18 @@ import ProfileBox from "@/app/(components)/ProfileBox";
 
 export default function Header() {
     return (
-          <div className="bg-black flex justify-between items-center flex-row w-full h-[70px] px-[20px]  pt-1 ">
-              <div className="flex items-center justify-center flex-row ">
-                  <Menu/>
-                  <Logo/>
-                  <HeaderNav/>
-              </div>
-              <div className="flex" >
-                  <SearchBox/>
-                  <ProfileBox/>
-              </div>
+        <div className="bg-black flex justify-between items-center flex-row w-full h-[70px] px-[20px]  pt-1 ">
+            <div className="flex items-center justify-center flex-row">
+                <Menu/>
+                <Logo/>
+                <HeaderNav/>
+            </div>
+            <div className="flex">
+                <SearchBox/>
+                <ProfileBox/>
+            </div>
 
-          </div>
+        </div>
     )
-  }
+}
   
--- app/(components)/HeaderNav.js
+++ app/(components)/HeaderNav.js
@@ -6,9 +6,9 @@ export default function HeaderNav () {
     return (
             <nav className="px-[15px]">
                 <ul className="mb-[10px]">
-                    <Link className="hidden no-underline text-white pl-[15px]" href="#"> خانه </Link>
-                    <Link className="hidden no-underline text-white pl-[15px]" href="#"> فیلم ها </Link>
-                    <Link className="hidden no-underline text-white pl-[15px]" href="#"> دسته بندی </Link>
+                    <Link className="ms:invisible cursor-pointer hover:text-[#6EB8FF] tab:visible no-underline text-white ml-[15px]" href="#"> خانه </Link>
+                    <Link className="ms:invisible cursor-pointer hover:text-[#6EB8FF] tab:visible no-underline text-white ml-[15px]" href="#"> فیلم ها </Link>
+                    <Link className="ms:invisible cursor-pointer hover:text-[#6EB8FF] tab:visible no-underline text-white ml-[15px]" href="#"> دسته بندی </Link>
                 </ul>
             </nav>
     )
