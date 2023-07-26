4f27fc885c7d78cf8d770919d43573cdd0088615
Mehrdad.Pashaei <mehrdad.pashaei1@gmail.com>
2023-07-26 03:14:32 -0700

    feat : added breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,13 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      'ms': '360px',
+      'ml': '500px',
+      'tab': '800px',
+      'des': '1280px',
+      'large': '1920px',
+    },
   },
   plugins: [],
 }
