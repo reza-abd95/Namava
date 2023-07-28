d108ad7cd9f1e24a085f5357c98d027cb07dd210
hasi-tara <tarahasiri@gmail.com>
2023-07-28 17:58:46 +0330

    feat(main slider): added card component for slider

--- Test.js
+++ Test.js
@@ -1,8 +1,18 @@
 import Logo from '/Logo';
+import Link from "next/link";
+import Image from "next/image";
+// import MobileImage from "/images/avatarMobile.png";
+// import DesktopImage from "/images/avatarMobile.png";
 export default  function Test(){
     return(
         <>
-<img src='./images/jang.jpg' className='relative'/>
+<Link href='#'><Image
+        //   className="w-[47px] h-[16px] des:w-[63px] des:h-[22px]"
+        width={1000}
+        height={1000}
+          src="/images/avatarMobile.jfif"
+          alt="avatar"
+        /></Link>
          <Logo /> 
         </>
     )
