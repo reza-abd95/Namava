5a40024c139b3d97a5cd3dcbbac2036d32589eab
hasi-tara <tarahasiri@gmail.com>
2023-07-28 17:58:18 +0330

    feat(main slider): added logo for slider

--- Logo.js
+++ Logo.js
@@ -1,9 +1,19 @@
-import Link from "next/link";
 
-export default function Logo () {
-    return (
-        <div className="absolute ">
-            <Link href="#"><img src='./images/logoj.png' alt="jang-e-jahani"/></Link>
-        </div>
-    )
-}
+import Link from "next/link";
+import Image from "next/image";
+// import LogoImage from "./images/avatarLogo.png";
+
+export default function Logo() {
+  return (
+    <div className="absolute">
+      <Link href="#">
+        <Image
+        //   className="w-[47px] h-[16px] des:w-[63px] des:h-[22px]"
+          src="/images/avatarLogo.png"
+          alt="avatar"
+          width={100}
+        height={100}
+        />
+      </Link>
+    </div>
+  )};
