37dce5108de7534d1a123900df4290d096b2d4a0
Mehrdad.Pashaei <mehrdad.pashaei1@gmail.com>
2023-07-29 23:05:48 -0700

    fix (categories) : fixed categories title on image.

--- app/categories/Category.js
+++ app/categories/Category.js
@@ -14,67 +14,80 @@ export default function Category() {
     return (
         <div className="w-full mt-[48px] flex flex-wrap justify-center items-center gap-[10px] ml:mt-[58px] ml:gap-[12px] tab:mt-[38px] tab:gap-[14px] des:mt-[58px] des:gap-[16px]  ">
             <Link className="no-underline text-white w-[43%] ml:w-[44%] tab:w-[29.5%] des:w-[22.4%]" href="#">
-                <div className="flex  w-[100%] h-[100%]">
+                <div className="flex  w-[100%] h-[100%]  align-middle items-center justify-start">
                     <Image className="relative w-[100%] h-[100%] rounded-[3px] des:rounded-[5px]" src={image1} alt="action"/>
-                    <h4 className="absolute mt-[30px] mr-[20px]">پردیس نماوا</h4>
+                    <h5 className="absolute mr-[25px]">پردیس نماوا</h5>
                 </div>
             </Link>
             <Link className="no-underline text-white w-[43%]  ml:w-[44%] tab:w-[29.5%] des:w-[22.4%]" href="#">
-                <div className="flex w-[100%] h-[100%]">
+                <div className="flex w-[100%] h-[100%]  align-middle items-center justify-start">
                     <Image className="relative w-[100%] h-[100%] rounded-[3px] des:rounded-[5px]" src={image2} alt="action"/>
-                    <h2 className="absolute mt-[30px] mr-[20px]">دوبله نماوا</h2>
+                    <h5 className="absolute mr-[25px]">دوبله نماوا</h5>
                 </div>
             </Link>
             <Link className="no-underline text-white w-[43%]  ml:w-[44%] tab:w-[29.5%] des:w-[22.4%]" href="#">
-                <div className="flex w-[100%] h-[100%]">
+                <div className="flex w-[100%] h-[100%]  align-middle items-center justify-start">
                     <Image className="relative w-[100%] h-[100%] rounded-[3px]" src={image3} alt="action"/>
-                    <h2 className="absolute mt-[30px] mr-[20px]">کودک</h2>
+                    <h5 className="absolute mr-[25px]">کودک</h5>
                 </div>
             </Link>
             <Link className="no-underline text-white w-[43%]  ml:w-[44%] tab:w-[29.5%] des:w-[22.4%]" href="#">
-                <div className="flex w-[100%] h-[100%]">
+                <div className="flex w-[100%] h-[100%]  align-middle items-center justify-start">
                     <Image className="relative w-[100%] h-[100%] rounded-[3px] des:rounded-[5px]" src={image4} alt="action"/>
-                    <h2 className="absolute mt-[30px] mr-[20px]">ایرانی</h2>
+                    <h5 className="absolute mr-[25px]">ایرانی</h5>
                 </div>
             </Link>
             <Link className="no-underline text-white w-[43%]  ml:w-[44%] tab:w-[29.5%] des:w-[22.4%]" href="#">
-                <div className="flex w-[100%] h-[100%]">
+                <div className="flex w-[100%] h-[100%]  align-middle items-center justify-start">
                     <Image className="relative w-[100%] h-[100%] rounded-[3px] des:rounded-[5px] " src={image5} alt="action"/>
-                    <h2 className="absolute mt-[30px] mr-[20px]">کره ای</h2>
+                    <h5 className="absolute mr-[25px]">کره ای</h5>
                 </div>
             </Link>
             <Link className="no-underline text-white w-[43%]  ml:w-[44%] tab:w-[29.5%] des:w-[22.4%]" href="#">
-                <div className="flex w-[100%] h-[100%]">
+                <div className="flex w-[100%] h-[100%]  align-middle items-center justify-start">
                     <Image className="relative w-[100%] h-[100%] rounded-[3px] des:rounded-[5px]" src={image6} alt="action"/>
-                    <h2 className="absolute mt-[30px] mr-[20px]">هندی</h2>
+                    <h5 className="absolute mr-[25px]">هندی</h5>
                 </div>
             </Link>
             <Link className="no-underline text-white w-[43%]  ml:w-[44%] tab:w-[29.5%] des:w-[22.4%]" href="#">
-                <div className="flex w-[100%] h-[100%]">
+                <div className="flex w-[100%] h-[100%]  align-middle items-center justify-start">
                     <Image className="relative w-[100%] h-[100%] rounded-[3px] des:rounded-[5px]" src={image6} alt="action"/>
-                    <h2 className="absolute mt-[30px] mr-[20px]">هندی</h2>
+                    <h5 className="absolute mr-[25px]">هندی</h5>
                 </div>
             </Link>
             <Link className="no-underline text-white w-[43%]  ml:w-[44%] tab:w-[29.5%] des:w-[22.4%]" href="#">
-                <div className="flex w-[100%] h-[100%]">
+                <div className="flex w-[100%] h-[100%]  align-middle items-center justify-start">
                     <Image className="relative w-[100%] h-[100%] rounded-[3px] des:rounded-[5px]" src={image6} alt="action"/>
-                    <h2 className="absolute mt-[30px] mr-[20px]">هندی</h2>
+                    <h5 className="absolute mr-[25px]">هندی</h5>
                 </div>
             </Link>
             <Link className="no-underline text-white w-[43%]  ml:w-[44%] tab:w-[29.5%] des:w-[22.4%]" href="#">
-                <div className="flex w-[100%] h-[100%]">
+                <div className="flex w-[100%] h-[100%]  align-middle items-center justify-start">
                     <Image className="relative w-[100%] h-[100%] rounded-[3px] des:rounded-[5px]" src={image6} alt="action"/>
-                    <h2 className="absolute mt-[30px] mr-[20px]">هندی</h2>
+                    <h5 className="absolute mr-[25px]">هندی</h5>
                 </div>
             </Link>
             <Link className="no-underline text-white w-[43%]  ml:w-[44%] tab:w-[29.5%] des:w-[22.4%]" href="#">
-                <div className="flex w-[100%] h-[100%]">
+                <div className="flex w-[100%] h-[100%]  align-middle items-center justify-start">
                     <Image className="relative w-[100%] h-[100%] rounded-[3px] des:rounded-[5px]" src={image6}
                            alt="action"/>
-                    <h2 className="absolute mt-[30px] mr-[20px]">هندی</h2>
+                    <h5 className="absolute mr-[25px]">هندی</h5>
                 </div>
             </Link>
-
+            <Link className="no-underline text-white w-[43%]  ml:w-[44%] tab:w-[29.5%] des:w-[22.4%]" href="#">
+                <div className="flex w-[100%] h-[100%]  align-middle items-center justify-start">
+                    <Image className="relative w-[100%] h-[100%] rounded-[3px] des:rounded-[5px]" src={image6}
+                           alt="action"/>
+                    <h5 className="absolute mr-[25px]">هندی</h5>
+                </div>
+            </Link>
+            <Link className="no-underline text-white w-[43%]  ml:w-[44%] tab:w-[29.5%] des:w-[22.4%]" href="#">
+                <div className="flex w-[100%] h-[100%] align-middle items-center justify-start">
+                    <Image className="relative w-[100%] h-[100%] rounded-[3px] des:rounded-[5px]" src={image6}
+                           alt="action"/>
+                    <h5 className="absolute mr-[25px]">هندی</h5>
+                </div>
+            </Link>
         </div>
 
     );
