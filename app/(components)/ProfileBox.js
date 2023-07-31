f7690fa22d6ff64705841d55499f137f9176eded
sam zeinali <saamzeinali@gmail.com>
2023-07-31 09:40:23 +0330

    feat (profileBox) : added Html

--- app/(components)/ProfileBox.js
+++ app/(components)/ProfileBox.js
@@ -10,8 +10,35 @@ export default function ProfileBox () {
     }
     return (
         <div>
-            <Image className="cursor-pointer w-[30px] h-[31px] rounded-full mt-[1px] mr-[8px] tab:h-[30px] tab:w-[31px] des:w-[40px] des:h-[40px]" src={Profile} alt="profile" onMouseOver={mouseOverHandler}/>
+            <div>
+                <Image className="cursor-pointer w-[30px] h-[31px] rounded-full mt-[1px] mr-[8px] tab:h-[30px] tab:w-[31px] des:w-[40px] des:h-[40px]" src={Profile} alt="profile" onMouseOver={mouseOverHandler}/>
+            </div>
+            <div>
+                <div>
+                    <div>
+                        <p></p>
+                        <button></button>
+                    </div>
+                    <div>
+                        <div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div></div>
+                        <div>
+                            <ul>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
+        
     )
 }
