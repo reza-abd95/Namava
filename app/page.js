109a55a21b9191c8224bf08a61c02683baa120e0
hasi-tara <tarahasiri@gmail.com>
2023-07-31 16:57:17 +0330

    feat(main slider): changing file names to prevent  conflicts

--- app/page.js
+++ app/page.js
@@ -1,7 +1,11 @@
-
+import Image from 'next/image'
+import Slider from './Slider'
+import Test from './Test'
 export default function Home() {
   return (
         <div>
+          {/* <Slider/> */}
+          <Test/>
           Home
         </div>
   )
