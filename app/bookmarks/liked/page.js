71c36800c3340ca2dfab45754b112d852f64a72b
Reza Abedi <reza.abd95@yahoo.com>
2023-08-02 01:08:46 +0330

    feat(bookmarks and liked) : added bookmarks and liked pages and their styles

---
+++ app/bookmarks/liked/page.js
@@ -0,0 +1,43 @@
+import EmptyBookmark from '@/app/(components)/EmptyBookmark';
+import MovieForSlider from '@/app/(components)/MovieForSlider'
+import React from 'react'
+
+export default function Liked() {
+
+  
+  let emptyDisplay = "";
+  let cardDisplay ="";
+
+  if (false){
+    emptyDisplay = "hidden" 
+    cardDisplay = "flex"
+  }else{
+    emptyDisplay = "flex"
+    cardDisplay = "hidden" 
+  }
+
+  
+  return (
+        <div className="min-h-[710px] pb-20">
+
+
+            <div className={`${emptyDisplay}`}>
+              <EmptyBookmark/>
+            </div>
+
+            <div className={` text-white w-full px-6 ml:px-7 des:px-10 ${cardDisplay} flex-wrap  items-center `}>
+               
+                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
+                    <MovieForSlider/>
+                </div> 
+               
+                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
+                    <MovieForSlider/>
+                </div> 
+                
+            </div>
+
+
+        </div>
+  )
+}
