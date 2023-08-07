33381d85aa399ec4f2a99efaefa33a9eabc45962
Mehrdad.Pashaei <mehrdad.pashaei1@gmail.com>
2023-08-07 05:36:54 -0700

    feat (categories & comments) : add categories data and fetching data. set onclick on comment input for reset input value.

--- app/categories/Category.js
+++ app/categories/Category.js
@@ -1,32 +1,24 @@
 
+
 import React from 'react';
 import CategoryItem from "@/app/(components)/Categoryitem";
 
-export default function Category() {
+export default function Category({data}) {
+        console.log("zart")
     return (
         <div className=" w-full mt-[48px] px-[20px] pb-[70px] flex flex-wrap justify-start  items-center gap-[10px] ml:mt-[58px] ml:gap-[12px] ml:px-[24px] tab:px-[32px] tab:mt-[38px] tab:gap-[14px] des:px-[43px] des:mt-[58px] des:gap-[16px] large:px-[55px] large:mt-[80px]">
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
-            <CategoryItem/>
+            {data.map((Items, key) => {
+                return (
+                    <CategoryItem
+                        key={Items.id}
+                        id={Items.id}
+                        name={Items.name}
+                        title={Items.title}
+                        image={Items.imageUrl}
+                    />
+                );
+            })}
+
         </div>
 
     );
