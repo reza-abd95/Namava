90af1a46546cd9a17e44d827e568f2655f0d9e77
Reza Abedi <reza.abd95@yahoo.com>
2023-08-07 22:06:44 +0330

    fix

--- app/search/page.js
+++ app/search/page.js
@@ -24,64 +24,10 @@ export default function Search() {
             <div className=" text-white w-full mt-[48px] px-6 ml:px-7 des:px-10 flex flex-wrap  items-center ">
 
                
-                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
+                {/* <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
                     <MovieForSlider/>
-                </div> 
-               
-                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
-                    <MovieForSlider/>
-                </div> 
-               
-                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
-                    <MovieForSlider/>
-                </div> 
-               
-                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
-                    <MovieForSlider/>
-                </div> 
-               
-                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
-                    <MovieForSlider/>
-                </div> 
-                
-               
-                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
-                    <MovieForSlider/>
-                </div> 
-                
-               
-                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
-                    <MovieForSlider/>
-                </div> 
-                
-               
-                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
-                    <MovieForSlider/>
-                </div> 
-                
-               
-                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
-                    <MovieForSlider/>
-                </div> 
-                
-               
-                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
-                    <MovieForSlider/>
-                </div> 
-                
-                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
-                    <MovieForSlider/>
-                </div> 
-                
-                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
-                    <MovieForSlider/>
-                </div> 
-                
-                <div className=" w-1/3 ml:w-1/4 tab:w-1/5  des:w-[14.2%]  px-1 py-4 ml:px-2 des:3">
-                    <MovieForSlider/>
-                </div> 
-                
-                
+                </div>  */}
+
                
                 
             </div>
