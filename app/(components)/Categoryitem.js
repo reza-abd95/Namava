0e67291e4aa2c75fdc904361d9f2aaeec66ae6f4
Reza Abedi <reza.abd95@yahoo.com>
2023-08-07 22:05:18 +0330

    fix(categories): fix some bugs

--- app/(components)/Categoryitem.js
+++ app/(components)/Categoryitem.js
@@ -9,7 +9,7 @@ export default function CategoryItem({id,name,title,image}) {
 
         <Link className="no-underline text-white w-[48%] ml:w-[48.5%] tab:w-[32%] des:w-[23.97%] large:w-[19.28%]" href={`/categories/${name}${id}`}>
             <div className="flex  w-[100%] h-[100%]  align-middle items-center justify-start">
-                <Image className="relative w-[100%] h-[100%] rounded-[3px] des:rounded-[5px]" src={image} alt={name} width={100} height={100}/>
+                <Image className="relative w-[100%] h-[100%] rounded-[3px] des:rounded-[5px]" src={image} alt={name} width={720} height={400}/>
                 <span className="absolute mr-[12px] font-iranyekanRegular text-[14px] ml:mr-[24px] ml:text-[18px] tab:text-[20px] ">{title}</span>
             </div>
         </Link>
