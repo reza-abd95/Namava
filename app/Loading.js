bdb69b1fd0e3c2afc6e940f0bd30baf1c0d6cea0
Reza Abedi <reza.abd95@yahoo.com>
2023-08-13 13:21:27 +0330

    added loading

--- app/Loading.js
+++ app/Loading.js
@@ -1,7 +1,7 @@
 export default function Loading () {
     return(
         <div className="w-full h-screen text-white text-3xl flex items-center justify-center align-center">
-            ...loading
+            ...loading 
         </div>
     )
 
