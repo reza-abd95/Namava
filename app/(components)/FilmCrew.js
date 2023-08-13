a7ece60e0255b31420a98bd0ca90c2cbbe3600cc
Reza Abedi <reza.abd95@yahoo.com>
2023-08-13 11:42:23 +0330

    fix : changed data fetching in sliders

--- app/(components)/FilmCrew.js
+++ app/(components)/FilmCrew.js
@@ -2,17 +2,17 @@
 
 import Image from "next/image"
 
-export default function FilmCrew(data) {
+export default function FilmCrew({data}) {
     return (
         <div>              
             <div>
                 <Image  className="block max-w-full h-auto rounded-full "
-                        src={data.data.imageUrl}
+                        src={data.imageUrl}
                         width={1000}
                         height={1000}
-                        alt={data.data.name} />
+                        alt={data.name} />
             </div>
-            <p className=" pt-3 text-xs">{data.data.name}</p>
+            <p className=" pt-3 text-xs">{data.name}</p>
         </div>
     )
   }
