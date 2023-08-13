da3e18d3ad8e99b0cbd520f8e97304ea98b6336e
Reza Abedi <reza.abd95@yahoo.com>
2023-08-13 13:39:23 +0330

    added loading to bookmarks

---
+++ app/bookmarks/loading.js
@@ -0,0 +1,9 @@
+export default function Loading () {
+    return(
+        <div className="w-full h-screen text-white text-3xl flex items-center justify-center align-center">
+            ...loading 
+        </div>
+    )
+
+
+}
