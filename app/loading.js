e2c67e30ccfe6834a56412392b4598b64ddc5a35
Mehrdad.Pashaei <mehrdad.pashaei1@gmail.com>
2023-08-13 05:38:39 -0700

    feat(FinalChange) : edited some styles.

--- app/loading.js
+++
@@ -1,9 +0,0 @@
-export default function Loading () {
-    return(
-        <div className="w-full h-screen text-white text-3xl flex items-center justify-center align-center">
-            ...loading 
-        </div>
-    )
-
-
-}
